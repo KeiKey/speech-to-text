925176173648a0d30765d475c4fbf3104bf0bebb
kei selami <kei.selami@eurostep.it>
2024-03-29 19:51:16 +0100

    Upgrade laravel project to version 10

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,9 +1,5 @@
 <?php
 
-use App\Http\Controllers\Api\V1\CompanyController;
-use App\Http\Controllers\Api\V1\IndividualController;
-use App\Http\Controllers\Api\V1\TransactionController;
-use App\Http\Controllers\Api\PassportAuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,17 +9,11 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
 |
 */
 
-Route::post('register', [PassportAuthController::class, 'register']);
-Route::post('login', [PassportAuthController::class, 'login']);
-
-// V1 API Routes
-Route::group(['prefix' => 'v1', 'middleware' => ['auth:api']], function () {
-    Route::apiResource('transactions', TransactionController::class, ['as' => 'api.v1'])->scoped(['transaction' => 'uuid']);
-    Route::apiResource('companies', CompanyController::class, ['as' => 'api.v1'])->scoped(['company' => 'uuid']);
-    Route::apiResource('individuals', IndividualController::class, ['as' => 'api.v1'])->scoped(['individual' => 'uuid']);
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
 });
